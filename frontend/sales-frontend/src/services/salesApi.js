a6e9a756940b872408fda8dc52c4ee5e296dd2ab
Thusharpoojary <thusharpoojary124@gmail.com>
2026-07-03 20:20:37 +0530

    api updated

--- frontend/sales-frontend/src/services/salesApi.js
+++ frontend/sales-frontend/src/services/salesApi.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE ='https://sales-report-project-1.onrender.com/api/sales';
 
+
 export const getKpis=()=>axios.get(`${BASE}/kips`);
 export const getByRegion=()=> axios.get(`${BASE}/by-region`);
 export const getByCategory=()=>axios.get(`${BASE}/by-category`);
